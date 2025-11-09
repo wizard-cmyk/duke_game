b091aac2db34599f8395c68779142c6ee6dd166e
wizard-cmyk <dallasmarquis@gmail.com>
2025-11-09 16:37:16 -0700

    Add files via upload

---
+++ duke-game/Scripts/Global Scripts/Global/Global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const SPRITE_GRID_SIZE: int = 64
+const MAP_CELL_MULTIPLIER: int = 2
+const DEFAULT_MAP_WIDTH: int = 4
+
+const MAP_SHAPE: int = 2
+
+const SUCCESS: bool = true
+const FAIL: bool = false
+
+const GRID_SPRITE: String = "res://Sprites/Grid/grid_element.png"
+const MONSTER_SPRITE: String = "res://Sprites/Monster/monster.png"
+const KNIGHT_SPRITE: String = "res://Sprites/Knight/knight.png"
---
+++ duke-game/Scripts/Global Scripts/SpriteInfo/SpriteInfo.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var monster_resource = preload(G.MONSTER_SPRITE)
+var grid_resource = preload(G.GRID_SPRITE)
